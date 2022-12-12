4bb589a9ea905af48f09dc99b655e215a2d9e20b
dburns37 <dburns37@gatech.edu>
2022-12-12 10:58:14 -0500

    advent 11 done!

--- advent11.py
+++ advent11.py
@@ -11,41 +11,41 @@ def solve():
         items = [int(i) for i in line.split(", ")]
         line = f.readline().replace("  Operation: new = ", "")
         line = line.replace("old", 'x')[:-1]
-        op = lambda x: x * x
+        num = 'x'
+        op = line[2]
         if line[4] != 'x':
             num = int(line[4:])
-            if line[2] == '*':
-                op = lambda x: x * num
-            elif line[2] == '+':
-                op = lambda x: x + num
         line = f.readline().replace("  Test: divisible by ", "")[:-1]
-        num = int(line)
-        test = lambda x: x % num == 0
+        test = int(line)
         monkeyT = int(f.readline().replace("    If true: throw to monkey ", "")[:-1])
         monkeyF = int(f.readline().replace("    If false: throw to monkey ", "")[:-1])
         f.readline()
 
-        monkeys.append(Monkey(items, op, test, monkeyT, monkeyF))
+        monkeys.append(Monkey(items, op, num, test, monkeyT, monkeyF))
 
+    divisible = 1
     for monkey in monkeys:
+        divisible *= monkey.test
         monkey.updatePassMonkeys(monkeys[monkey.monkeyT], monkeys[monkey.monkeyF])
+    print(divisible)
     
-    for i in range(20):
+    for i in range(10000):
+        if i % 1000 == 0:
+            print(i)
         for monkey in monkeys:
             monkey.simulate()
     
     monkeys.sort(key=lambda x: x.numPasses)
-    for m in monkeys:
-        print(m.numPasses)
     return monkeys[-1].numPasses * monkeys[-2].numPasses
     
 
 
 class Monkey:
-    def __init__(self, items, operation, test, monkeyT, monkeyF):
+    def __init__(self, items, operation, num, test, monkeyT, monkeyF):
         self.items = items
         self.operation = operation
         self.test = test
+        self.num = num
         self.monkeyT = monkeyT
         self.monkeyF = monkeyF
         self.numPasses = 0
@@ -58,9 +58,14 @@ class Monkey:
         while len(self.items) > 0:
             self.numPasses += 1
             item = self.items.pop(0)
-            cost = int(self.operation(item) / 3)
-            if self.test(cost):
-                self.monkeyT.items.append(cost)
+            cost = item * item
+            if self.num != 'x': 
+                if self.operation == '*':
+                    cost = self.num * item
+                else:
+                    cost = self.num + item
+            if (cost % self.test) == 0:
+                self.monkeyT.items.append((cost % 9699690))
             else:
                 self.monkeyF.items.append(cost)
 
